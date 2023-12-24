0db69be27e16b6f3462030ce562e4d7996d7d72c
Aditya <85586593+Aditya94505@users.noreply.github.com>
2023-12-24 14:50:41 +0530

    Update AKTU 1.c

--- AKTU 1.c
+++ AKTU 1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <conio.h>
 void main()
 {
 	int s1,s2,s3,s4,s5,sum,percentage;
+	clrscr();
 	printf("Sum and percentage Calculator Of Five Subjects");
 	printf("\n\nEnter the marks obtained in Subject 1: ");
 	scanf("%d",&s1);
@@ -17,4 +19,5 @@ void main()
 	printf("\nStudent obtained %d marks out of 500",sum);
 	percentage=sum*100/500;
 	printf("\nStudent obtained %d percentage",percentage);
+	getch();
 }
