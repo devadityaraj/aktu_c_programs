aba5269f948303031e95f617ace4c2312b8c81f4
Aditya <85586593+Aditya94505@users.noreply.github.com>
2023-12-24 14:55:26 +0530

    Update AKTU 6.c

--- AKTU 6.c
+++ AKTU 6.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <conio.h>
 
-void  main() {
+void  main()
+{
     int num1, num2;
-clrscr();
-    // Input first number
+    clrscr();
     printf("Enter the first number: ");
     scanf("%d", &num1);
-
-    // Input second number
     printf("Enter the second number: ");
     scanf("%d", &num2);
-
-    // Check if the numbers are equal
     if (num1 == num2) {
         printf("The entered numbers are equal.\n");
     } else {
         printf("The entered numbers are not equal.\n");
     }
-
     getch();
 }
