96e625cd9c7278a59e883ac874f49862cebbeafa
Aditya <85586593+Aditya94505@users.noreply.github.com>
2023-12-24 14:53:58 +0530

    Update AKTU 4.c

--- AKTU 4.c
+++ AKTU 4.c
@@ -3,14 +3,9 @@
 void main() {
     float celsius, fahrenheit;
     clrscr();
-    // Input temperature in Celsius
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
-
-    // Convert Celsius to Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
-
-    // Display the result
     printf("%.2f Celsius is equal to %.2f Fahrenheit\n", celsius, fahrenheit);
     getch();
 }
