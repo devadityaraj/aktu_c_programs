325fb77788ab0ad2be148d864971d49d90bc9b77
Aditya <85586593+Aditya94505@users.noreply.github.com>
2023-12-28 15:56:51 +0530

    Create AKTU 15.c

---
+++ AKTU 15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+void main ()
+{
+  int terms, first, second, next;
+int i;
+clrscr();
+first = 0;
+second = 1;
+printf("Enter the number of terms in thr Fibonacci Series: ");
+scanf("%d",&terms);
+printf("\nFABNOCII SERIES");
+for(i=0; i<terms; i++)
+{
+printf("\n%d",first);
+next = first + second;
+first = second;
+second = next;
+}
+getch();
+}
