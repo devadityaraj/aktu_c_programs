881824776c8db4673cd8398a644ed1e428d07253
Aditya <85586593+Aditya94505@users.noreply.github.com>
2023-12-29 11:19:54 +0530

    Update AKTU 16.c

--- AKTU 16.c
+++ AKTU 16.c
@@ -1,7 +1,9 @@
 #include <stdio.h> 
+#include <conio.h>
 void main()
 { 
 int num,i,r,s;
+clrscr();
 printf("Enter the number: ");
 scanf("%d",&num);
 r=0;
@@ -12,9 +14,10 @@ for(i=num/2; i>1; i--) {
     }
 }
 if(r==1){
-    printf("%d is a prime number",num);
-}
-else {
     printf("%d is not a prime number",num);
 }
+else {
+    printf("%d is a prime number",num);
+}
+    getch();
 }
