69dad30012779062c2050fe1bb6db29b74fd9282
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-02 19:35:28 +0530

    Update AKTU 9.c

--- AKTU 9.c
+++ AKTU 9.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 #include <conio.h>
-
 void main() {
     int year;
     clrscr();
     printf("Enter a year: ");
     scanf("%d", &year);
     if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                printf("%d is a leap year.\n", year);
-            } else {
-                printf("%d is not a leap year.\n", year);
-            }
-        } else {
             printf("%d is a leap year.\n", year);
         }
-    } else {
+     else {
         printf("%d is not a leap year.\n", year);
     }
-getch();
+    getch();
 }
