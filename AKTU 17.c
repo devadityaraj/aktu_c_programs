67cccd7ce743ef681a01205022b382bcb224c75a
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-03 19:24:32 +0530

    Update AKTU 17.c
    bug fixed

--- AKTU 17.c
+++ AKTU 17.c
@@ -5,14 +5,15 @@ void main() {
     int num, digit;
     int sum=0;
     clrscr();
-    printf("Enter number to find the sum of digits: ");
-    scanf("%d", &num);
+    printf("Enter number of Any Digits: ");
+    scanf("%d",&num);
     
     while (num > 0) {
         digit = num % 10;
         sum += digit;
+        num /= 10;
     }
-    printf("Sum of the digits: %d\n", sum);
+    printf("Sum of the digits: %d\n",sum);
 
  getch();
        
