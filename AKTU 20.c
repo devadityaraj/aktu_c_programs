bdd7652a67de9de92e0665036c105f47b60133ae
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-04 18:13:18 +0530

    Update AKTU 20.c
    Choice added to pick any one

--- AKTU 20.c
+++ AKTU 20.c
@@ -1,1 +1,45 @@
-//binary to decimal and vice versa
+#include <stdio.h>
+#include <conio.h>
+void main()
+{   
+    int choice;
+    clrscr();
+    printf("Enter 1 for decimal to binary or 2 for binary to decimal: ");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        int decimal, remain, base;
+        long long binary;
+        binary = 0;
+        base = 1;
+        printf("Enter the decimal number: ");
+        scanf("%d",&decimal);
+        while(decimal > 0)
+        {
+            remain = decimal % 2;
+            binary += remain * base;
+            decimal /= 2;
+            base *= 10;
+        }
+        printf("Binary number for %d is: %lld",decimal,binary);
+    }
+    else if(choice==2) {
+        long long binary;
+        int decimal, remain, base;
+        decimal = 0;
+        base = 1;
+        printf("Enter the binary number: ");
+        scanf("%lld",&binary);
+        while(binary > 0) {
+            remain = binary % 10;
+            decimal += remain * base;
+            binary /= 10;
+            base *= 2;
+            }
+        printf("Decimal number for %lld is: %d",binary,decimal);
+    }
+    else {
+        printf("Please enter the valid choice only.\n");
+    }
+  getch();
+}
