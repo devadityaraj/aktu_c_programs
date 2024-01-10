4ab7128c56cb7d1f573925c4383c1bb2af6bb2b5
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-10 21:17:44 +0530

    Update bank.c

--- bank.c
+++ bank.c
@@ -19,9 +19,11 @@ void main() {
     scanf("%s",&password);
     int value=strcmp(id,ID);  
    if(value==0)  {
+       int value=strcmp(password,PASSWORD);  
+   if(value==0) {
    printf("\n\nLogin Success!!");
    do {
-   printf("\n\nWelcome, MASTER Aditya Raj! \n\n1. Add Money \n2. Withdraw Money \n3. Check Balance \n4. Exit \n\nEnter Your Choice Sir: ");
+   printf("\n\nWelcome, MASTER Aditya Raj! \n\n1. Add Money \n2. Withdraw Money \n3. Check Balance \n4. Logout \n\nEnter Your Choice Sir: ");
    scanf("%d",&choice);
    switch(choice) {
        case 1:
@@ -31,38 +33,57 @@ void main() {
            scanf("%f",&add_ammount);
            balance += add_ammount;
            printf("\n\n%0.2f is addedd to your account!",add_ammount);
-           printf("\n\nTotal balance is: %f",balance);
+           printf("\n\nTotal balance is: %0.2f",balance);
        }
      break;
      
      case 2:
-     {
+     { 
+         if(balance < 500) {
+             printf("Not enough balance to withdraw\nPlease maintain 500 minimum balance\nYour balance is: %0.2f",balance);
+         }
+         else {
          float subtract_ammount;
-           printf("Enter the ammount to be added: ");
+           printf("Enter the ammount to withdraw: ");
            scanf("%f",&subtract_ammount);
-           balance += subtract_ammount;
-           printf("\n\n%0.2f is addedd to your account!",subtract_ammount);
-           printf("\n\nTotal balance is: %f",balance);
+           if(subtract_ammount > balance-500) {
+               printf("\n\nPlease Maintain Your Minimum Balance (500) \nYour balance is %0.2f",balance);
+           }
+           else {
+           balance -= subtract_ammount;
+           printf("\n\n%0.2f is withdrawed from your account!\n\nRemaining balance: ",subtract_ammount,balance);
+           printf("\n\nTotal balance is: %0.2f",balance);
+           }
+         }
      }
     break;
     
     case 3:
     {
-        printf("Check Balance");
+        printf("Your Current Account Balance is: %0.2f",balance);
     }
     
     case 4:
     {
-        printf("Exiting");
+        printf("Logout Sucess");
+    }
+    
+    default: {
+        printf("Please Give Valid Choice");
     }
        
    }
    
    
-   }  while(choice != 4);
-
-} else  {
-   printf("Something Went Wrong");
+   }  while(choice = 4);
+} else {
+    printf("Wrong User ID or Password \n\n");
+    main();
 }
+} else  {
+   printf("Wrong User ID or Password \n\n");
+   main();
+}
+
 }
 
