f8ce8d2a4e23f9a474ade46592eb1bf7914f015f
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-10 11:18:21 +0530

    Update AKTU22.c

--- AKTU22.c
+++ AKTU22.c
@@ -27,6 +27,6 @@
       array3[2]=sum2;
       printf("\nThe sum of both arrays are: ");
           printf("%d ",array3[1]);
-          printf(" %d ",array3[2]);
+          printf("%d",array3[2]);
     getch();
   }
