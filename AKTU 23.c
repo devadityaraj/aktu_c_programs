4cd03a68ec6ead08c41a8b288feb80521dfd6c11
Aditya <85586593+Aditya94505@users.noreply.github.com>
2024-01-16 19:53:45 +0530

    Update AKTU 23.c

--- AKTU 23.c
+++ AKTU 23.c
@@ -1,26 +1,26 @@
-
-void main()
-{    
-    int array[100], maximum, size, c, location = 1;
-    
-    printf("Enter the number of elements in array\n");
+#include <stdio.h>
+#include <conio.h>
+void main() {
+    int array[100];
+    int i, max, min, size;
+    clrscr();
+    printf("Enter size of the array: ");
     scanf("%d", &size);
-    
-    printf("Enter %d integers\n", size);
-    
-    for (c = 0; c < size; c++)
-        scanf("%d", &array[c]);
-    
-    maximum = array[0];
-    
-    for (c = 1; c < size; c++)
-    {
-        if (array[c] > maximum)
-        {
-                maximum  = array[c];
-                location = c+1;
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++) {
+        scanf("%d", &array[i]);
+    }
+    max = array[0];
+    min = array[0];
+    for(i=1; i<size; i++) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+        if(array[i] < min) {
+            min = array[i];
         }
     }
-    
-    printf("Maximum element is present at location %d and it's value is %d.\n", location, maximum);
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d", min);
+    getch();
 }
